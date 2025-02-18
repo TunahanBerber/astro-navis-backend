042a0eb6cd7be6bab1c54d93757404bb9a738782
tunahan.berber <tunahan.berber@eba.gov.tr>
2025-02-18 16:39:05 +0300

    mail-service-update

--- src/models/emailModel.js
+++ src/models/emailModel.js
@@ -1,17 +1,11 @@
 const mongoose = require('mongoose');
 
+// Email şeması
 const emailSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
-    unique: true,
-    trim: true,
-    lowercase: true,
-    match: [/.+\@.+\..+/, 'Please fill a valid email address']
-  },
-  subscribedAt: {
-    type: Date,
-    default: Date.now
+    unique: true // Her emailin yalnızca bir kez kaydedilmesini sağla
   }
 });
 
